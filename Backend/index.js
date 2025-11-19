7e30b4a949229753def629b592324268b00fc95c
Geophrey <jeffkouevi@gmail.com>
2025-11-19 11:13:22 -0500

    frontend connected to backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors"
 
 const app = express();
 
 const PORT = process.env.PORT
+
+app.use(cors())
 
 //http://localhost:7777/
 app.get(`/`, (req, res) => {
