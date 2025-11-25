862ce4f48a23077d79e1660c6a245ed0a89b123f
Geophrey <jeffkouevi@gmail.com>
2025-11-25 04:54:12 -0500

    added simple css for calendar component

--- Frontend/src/components/Calendar.jsx
+++ Frontend/src/components/Calendar.jsx
@@ -10,7 +10,7 @@ export default function Calendar() {
 
                 <div>
                     <button>{"<"}</button>
-                    <span className="month">december</span>
+                    <span className="month">December</span>
                     <button>{">"}</button>
                 </div>
             </section>
@@ -20,56 +20,56 @@ export default function Calendar() {
                     <tr className="daysOfTheWeek">
                         <th className="day" id="sunday">Sunday</th>
                         <th className="day" id="monday">Monday</th>
-                        <th className="day" id="tuesday">Tueday</th>
-                        <th className="day" id="wednesday">Wedday</th>
-                        <th className="day" id="thursday">Thuday</th>
+                        <th className="day" id="tuesday">Tuesday</th>
+                        <th className="day" id="wednesday">Wednesday</th>
+                        <th className="day" id="thursday">Thursday</th>
                         <th className="day" id="friday">Friday</th>
-                        <th className="day" id="saturday">Satday</th>
+                        <th className="day" id="saturday">Saturday</th>
                     </tr>
                     <tr className="week1">
-                        <td className="week1Day" id="sunday">Sun</td>
-                        <td className="week1Day" id="monday">Mon</td>
-                        <td className="week1Day" id="tuesday">Tue</td>
-                        <td className="week1Day" id="wednesday">Wed</td>
-                        <td className="week1Day" id="thursday">Thur</td>
-                        <td className="week1Day" id="friday">Fri</td>
-                        <td className="week1Day" id="saturday">Sat</td>
+                        <td className="week1Day" id="sunday"><button>30</button></td>
+                        <td className="week1Day" id="monday"><button>1</button></td>
+                        <td className="week1Day" id="tuesday"><button>2</button></td>
+                        <td className="week1Day" id="wednesday"><button>3</button></td>
+                        <td className="week1Day" id="thursday"><button>4</button></td>
+                        <td className="week1Day" id="friday"><button>5</button></td>
+                        <td className="week1Day" id="saturday"><button>6</button></td>
                     </tr>
                     <tr className="week2">
-                        <td className="week2Day" id="sunday">Sun</td>
-                        <td className="week2Day" id="monday">Mon</td>
-                        <td className="week2Day" id="tuesday">Tue</td>
-                        <td className="week2Day" id="wednesday">Wed</td>
-                        <td className="week2Day" id="thursday">Thur</td>
-                        <td className="week2Day" id="friday">Fri</td>
-                        <td className="week2Day" id="saturday">Sat</td>
+                        <td className="week2Day" id="sunday"><button>7</button></td>
+                        <td className="week2Day" id="monday"><button>8</button></td>
+                        <td className="week2Day" id="tuesday"><button>9</button></td>
+                        <td className="week2Day" id="wednesday"><button>10</button></td>
+                        <td className="week2Day" id="thursday"><button>11</button></td>
+                        <td className="week2Day" id="friday"><button>12</button></td>
+                        <td className="week2Day" id="saturday"><button>13</button></td>
                     </tr>
                     <tr className="week3">
-                        <td className="week3Day" id="sunday">Sun</td>
-                        <td className="week3Day" id="monday">Mon</td>
-                        <td className="week3Day" id="tuesday">Tue</td>
-                        <td className="week3Day" id="wednesday">Wed</td>
-                        <td className="week3Day" id="thursday">Thur</td>
-                        <td className="week3Day" id="friday">Fri</td>
-                        <td className="week3Day" id="saturday">Sat</td>
+                        <td className="week3Day" id="sunday"><button>14</button></td>
+                        <td className="week3Day" id="monday"><button>15</button></td>
+                        <td className="week3Day" id="tuesday"><button>16</button></td>
+                        <td className="week3Day" id="wednesday"><button>17</button></td>
+                        <td className="week3Day" id="thursday"><button>18</button></td>
+                        <td className="week3Day" id="friday"><button>19</button></td>
+                        <td className="week3Day" id="saturday"><button>20</button></td>
                     </tr>
                     <tr className="week4">
-                        <td className="week4Day" id="sunday">Sun</td>
-                        <td className="week4Day" id="monday">Mon</td>
-                        <td className="week4Day" id="tuesday">Tue</td>
-                        <td className="week4Day" id="wednesday">Wed</td>
-                        <td className="week4Day" id="thursday">Thur</td>
-                        <td className="week4Day" id="friday">Fri</td>
-                        <td className="week4Day" id="saturday">Sat</td>
+                        <td className="week4Day" id="sunday"><button>21</button></td>
+                        <td className="week4Day" id="monday"><button>22</button></td>
+                        <td className="week4Day" id="tuesday"><button>23</button></td>
+                        <td className="week4Day" id="wednesday"><button>24</button></td>
+                        <td className="week4Day" id="thursday"><button>25</button></td>
+                        <td className="week4Day" id="friday"><button>26</button></td>
+                        <td className="week4Day" id="saturday"><button>27</button></td>
                     </tr>
                     <tr className="week5">
-                        <td className="week5Day" id="sunday">Sun</td>
-                        <td className="week5Day" id="monday">Mon</td>
-                        <td className="week5Day" id="tuesday">Tue</td>
-                        <td className="week5Day" id="wednesday">Wed</td>
-                        <td className="week5Day" id="thursday">Thur</td>
-                        <td className="week5Day" id="friday">Fri</td>
-                        <td className="week5Day" id="saturday">Sat</td>
+                        <td className="week5Day" id="sunday"><button>28</button></td>
+                        <td className="week5Day" id="monday"><button>29</button></td>
+                        <td className="week5Day" id="tuesday"><button>30</button></td>
+                        <td className="week5Day" id="wednesday"><button>31</button></td>
+                        <td className="week5Day" id="thursday"><button>1</button></td>
+                        <td className="week5Day" id="friday"><button>2</button></td>
+                        <td className="week5Day" id="saturday"><button>3</button></td>
                     </tr>
                 </table>
             </section>
